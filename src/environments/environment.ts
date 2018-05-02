1f4157097e19e22045c455aded755acdf48676d3
rcurnow1 <ryan.curnow@live.co.uk>
2018-05-02 13:21:46 +0100

    Add files via upload
    init commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  token: 'a79dce4ab6f14dd28af09ac632eb9cd6'
+};
