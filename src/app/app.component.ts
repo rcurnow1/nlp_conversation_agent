af35e0b90c4772fbb59590ede2c31ac25ef09a65
rcurnow1 <ryan.curnow@live.co.uk>
2018-05-28 12:13:00 +0100

    3rd initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Message } from '@app/models';
+//import * as p5 from 'p5';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,10 @@ export class AppComponent {
   constructor(){
     this.message = new Message('', 'assets/images/user.png');
     this.messages = [
-      new Message('Welcome to chatbot universe', 'assets/images/bot.png', new Date())
+      new Message('Hi names Lisa the Listening Agent, whats yours?', 'assets/images/bot.png', new Date()),
+      new Message('I am a friendly conversational agent, I provide an anonymous listening post for you to interact with if you need someone to talk to', 'assets/images/bot.png', new Date()),
+      new Message('To do this I use IBM Watson™ tone analyser to better understand the tone of the conversation with the individual', 'assets/images/bot.png', new Date()),
+      new Message('Bare with me when conversing as I am currently in beta mode so some interactions maybe not be in my databank', 'assets/images/bot.png', new Date())
     ];
   }
 }
